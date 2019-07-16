fb53981917466d2995cdf915a9538668c9b590a8
banriikku <banriikku.ts@gmail.com>
2019-07-16 12:34:13 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
     @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
 
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user # 保存成功後、ログインします。
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # get 'users/new' ← 「rails g controller Users new」実行時に挿入
 
   # get 'static_pages/top'
   root 'static_pages#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 
 end
