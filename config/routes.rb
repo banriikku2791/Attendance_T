63112d6175d7bb05c71c8f16b41e2ff202247565
banriikku <banriikku.ts@gmail.com>
2019-07-16 07:44:34 +0000

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # get 'users/new' ← 「rails g controller Users new」実行時に挿入
+
   # get 'static_pages/top'
   root 'static_pages#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+
 end
